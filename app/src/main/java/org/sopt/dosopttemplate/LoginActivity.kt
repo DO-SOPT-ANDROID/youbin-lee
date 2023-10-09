33b9c501eb575bd94977c0ec1f20655614e2f694
leeeyubin <youbeene3409@naver.com>
2023-10-09 15:56:38 +0900

    [UI/#1] activity_sign_up.xml UI 완료

--- app/src/main/java/org/sopt/dosopttemplate/LoginActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/LoginActivity.kt
@@ -1,11 +1,21 @@
 package org.sopt.dosopttemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.sopt.dosopttemplate.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.loginSignupBtn.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
