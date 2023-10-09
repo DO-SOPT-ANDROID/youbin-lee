02a44400b4b93b59cba238cdb3cfb585659861c2
leeeyubin <youbeene3409@naver.com>
2023-10-09 17:25:41 +0900

    [FEAT/#1] SignUpActivity 기능 구현

--- app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -1,11 +1,39 @@
 package org.sopt.dosopttemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.sopt.dosopttemplate.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpSignupBtn.setOnClickListener{
+            val id = binding.signUpIdEt.text.toString()
+            val password = binding.signUpPwEt.text.toString()
+            val nickname = binding.signUpNicknameEt.text.toString()
+            val mbti = binding.signUpMbtiEt.text.toString()
+            var check : Boolean = true
+
+            if(id.isEmpty() || id.length < 6 || id.length > 10) check = false
+            if(password.isEmpty() || password.length < 8 || password.length > 12) check = false
+            if(nickname.isEmpty()) check = false
+            if(mbti.isEmpty()) check = false
+
+            if(check){
+                Toast.makeText(this, "회원가입 성공!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "정보를 다시 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
+
 }
