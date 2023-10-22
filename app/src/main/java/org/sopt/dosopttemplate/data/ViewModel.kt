cd4447f07d1c64e14d444900ed23e3d3759c624c
leeeyubin <youbeene3409@naver.com>
2023-10-23 00:08:01 +0900

    [ADD/#3] 아이템 추가

--- app/src/main/java/org/sopt/dosopttemplate/data/ViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/ViewModel.kt
@@ -8,23 +8,46 @@ class ViewModel : ViewModel() {
     val mockFriend = listOf<FriendSealed>(
         FriendSealed.MyProfile(
             profileImage = R.drawable.iv_my_profile,
-            name = "이유빈",
+            name = "이유빈"
         ),
-        FriendSealed.FriendProfile(
+        FriendSealed.FriendMusic(
             profileImage = R.drawable.iv_friend_profile1,
             name ="SOPT OB 손흥민",
-            self_description = "이것이 축구다"
+            self_description = "이것이 축구다",
+            music = "Hype Boy - 뉴진스"
         ),
         FriendSealed.FriendProfile(
             profileImage = R.drawable.iv_friend_profile2,
             name ="SOPT OB 이강인",
-            self_description = "배고파"
+            self_description = null
         ),
         FriendSealed.FriendMusic(
             profileImage = R.drawable.iv_friend_profile3,
             name ="SOPT YB 정우영",
-            self_description = "배고파",
-            music = "Haru Haru - 빅뱅",
+            self_description = null,
+            music = "Haru Haru - 빅뱅"
+        ),
+        FriendSealed.FriendMusic(
+            profileImage = R.drawable.iv_friend_profile4,
+            name ="SOPT YB 해리케인",
+            self_description = "줄게 내 Galaxy,,",
+            music = "우주를 줄게 - 볼사"
+        ),
+        FriendSealed.FriendProfile(
+            profileImage = R.drawable.iv_friend_profile5,
+            name ="SOPT YB 살라",
+            self_description = "일주일 동안 연락 안 돼요"
+        ),
+        FriendSealed.FriendMusic(
+            profileImage = R.drawable.iv_friend_profile6,
+            name ="SOPT YB 엘링 홀란",
+            self_description = "뉴진스의 하입보이요",
+            music = "You & Me - JENNIE"
+        ),
+        FriendSealed.FriendProfile(
+            profileImage = R.drawable.iv_friend_profile7,
+            name ="SOPT YB 기성용",
+            self_description = null
         )
     )
 }
