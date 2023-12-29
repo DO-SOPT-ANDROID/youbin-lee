0c3205591a006564408db2ca9592a96d0b32c7d1
LEE YOU BIN <youbeene3409@naver.com>
2023-12-30 02:44:19 +0900

    [MOD/#18] 코드리뷰 반영

--- app/src/main/java/org/sopt/dosopttemplate/presentation/auth/LoginActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/auth/LoginActivity.kt
@@ -54,7 +54,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initLoginBtnListener() {
-        binding.btnLoginLogin.setOnClickListener {
+        binding.btnLoginLogin.setOnSingleClickListener {
             loginViewModel.checkLoginAvailable()
         }
     }
